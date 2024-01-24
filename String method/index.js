4bb5c44572cc9abea12cd1f0dfd192d7ceb763b5
Breylu <mihael.p.petrov05@gmail.com>
2024-01-24 22:22:34 +0200

    String slicing

--- String method/index.js
+++ String method/index.js
@@ -1,10 +1,10 @@
-let userName = "Breylu";
+// let userName = "Breylu";
 
-console.log(userName.charAt(0));
+// console.log(userName.charAt(0));
 
-console.log(userName.indexOf("e"));
+// console.log(userName.indexOf("e"));
 
-console.log(userName.length);
+// console.log(userName.length);
 
 // userName = userName.trim();
 
@@ -18,3 +18,22 @@ console.log(userName.length);
 // let phoneNumber = "123-456-7890";
 
 // phoneNumber = phoneNumber.replaceAll("-", "");
+
+
+// String slicing
+
+// const fullName = "Bleedaxe Breylu"
+
+// let firstName = fullName.slice(0, 7);
+// let lastName = fullName.slice(8, 14);
+
+// let firsChar = fullName.slice(0, 1);
+// let lastChar = fullName.slice(-1);
+
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+// const email = "Breylu@gmail.com";
+
+// let userName = email.slice(0, email.indexOf("@"));
+// let extention = email.slice(email.indexOf("@") + 1);
