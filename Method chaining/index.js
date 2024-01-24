fb0a11d923188f5d4db2520d80660b4ac3b8146e
Breylu <mihael.p.petrov05@gmail.com>
2024-01-24 22:33:34 +0200

    Method chaining

---
+++ Method chaining/index.js
@@ -0,0 +1,15 @@
+let username = window.prompt("Enter your username: ");
+
+// ------- NO METHOD CHAINING --------
+
+// username = username.trim();
+// let letter = username.charAt(0);
+// letter = letter.toUpperCase();
+
+// let extraChars = username.slice(1);
+// extraChars = extraChars.toLowerCase();
+// username = letter + extraChars;
+
+// ------- METHOD CHAINING --------
+
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
