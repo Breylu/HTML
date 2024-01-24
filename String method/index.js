3ea0a21a286bc750937db00387b05d29e42e08d0
Breylu <mihael.p.petrov05@gmail.com>
2024-01-24 22:13:42 +0200

    String methods

---
+++ String method/index.js
@@ -0,0 +1,20 @@
+let userName = "Breylu";
+
+console.log(userName.charAt(0));
+
+console.log(userName.indexOf("e"));
+
+console.log(userName.length);
+
+// userName = userName.trim();
+
+// console.log(userName);
+
+// let result = userName.includes(" ");
+
+
+// removes all of the dashes
+
+// let phoneNumber = "123-456-7890";
+
+// phoneNumber = phoneNumber.replaceAll("-", "");
