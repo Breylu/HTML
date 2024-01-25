e7c99a9c4cfa6b1c6a0f585067d3d825206c587e
Breylu <mihael.p.petrov05@gmail.com>
2024-01-25 20:13:10 +0200

    For and while loop

---
+++ For and while loops/index.js
@@ -0,0 +1,43 @@
+// ------- WHILE LOOP -------
+
+// let username = "";
+
+// while(username === "" || username === null){
+//     username = window.prompt(`Enter your name`);
+// }
+
+// EXECUTES CODE AT LEAST ONCE
+
+// do{
+//     username = window.prompt(`Enter your name`);
+// }while(username === "" || username === null)
+
+// let loggedIn = false;
+// let username1;
+// let password;
+
+// while (!loggedIn) {
+//   username1 = window.prompt(`Enter your name`);
+//   password = window.prompt(`Enter your password`);
+
+//   if (username1 === "myUsername" && password === "myPassword") {
+//     loggedIn = true;
+//     console.log("You are logged in!");
+//   } else {
+//     console.log("invalid credentials! Please try again.");
+//   }
+// }
+
+// ------- FOR LOOP -------
+
+// for (let i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i == 13) {
+//     break;
+//   } else {
+//     console.log(i);
+//   }
+// }
