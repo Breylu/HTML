fc7c5289b069125bb6fd26f8b6acf0b8f3f76e35
Breylu <mihael.p.petrov05@gmail.com>
2024-01-25 20:53:06 +0200

    Functions

---
+++ Functions/index.js
@@ -0,0 +1,33 @@
+// function happyBirthday(username, age) {
+//   console.log("Happy birthday to you!");
+//   console.log("Happy birthday to you!");
+//   console.log(`Happy birthday dear ${username}!`);
+//   console.log("Happy birthday to you!");
+//   console.log(`You are ${age} years old `);
+// }
+
+// happyBirthday("Plankton", 13);
+// happyBirthday("Patrick", 55);
+
+// function add(x, y) {
+//   return x + y;
+// }
+// function subtract(x, y) {
+//   return x - y;
+// }
+
+// console.log(add(2, 3));
+// console.log(subtract(2, 3));
+
+// function isEven(number) {
+//   return number % 2 === 0 ? true : false;
+// }
+// console.log(isEven(3));
+
+// function isValidEmail(email) {
+//   if (email.includes("@")) {
+//     return email.includes("@") ? true : false;
+//   }
+// }
+
+// console.log(isValidEmail("notScam@gmail.ru"));
