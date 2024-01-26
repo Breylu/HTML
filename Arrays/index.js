0fd281a6a40c5483d9ad24bbb622936f2395cd7f
Breylu <mihael.p.petrov05@gmail.com>
2024-01-26 22:18:43 +0200

    Arrays and spread operator

---
+++ Arrays/index.js
@@ -0,0 +1,32 @@
+// let fruits = ["apple", "orange", "banana", "coconut"];
+
+// fruits.push("coconut");
+// fruits.pop();
+// fruits.unshift("mango");
+// fruits.shift();
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("apple");
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// for (let i = fruits.length - 1; i >= 0; i--) {
+//   console.log(fruits[i]);
+// }
+
+// --------- 2D ARRAYS ---------
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// matrix[0][0] = "X";
+// matrix[0][1] = "O";
+
+// for (let row of matrix) {
+//   const rowString = row.join(" ");
+//   console.log(rowString);
+// }
---
+++ Spread operator/index.js
@@ -0,0 +1,13 @@
+// let numbers = [1, 2, 3, 4, 5];
+// let maximum = Math.max(...numbers);
+
+// let username = "Breylu";
+// let letters = [...username];
+
+// console.log(letters);
+
+let fruits = ["apple", "orange", "tomato"];
+let vegetables = ["carrots", "celery", "potatoes"];
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(foods);
