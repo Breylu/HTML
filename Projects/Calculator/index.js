880319fae2b9930d1b3ad49dcf8486c58f45852a
Breylu <mihael.p.petrov05@gmail.com>
2024-01-29 15:15:24 +0200

    Function expressions

--- Projects/Calculator/index.js
+++ Projects/Calculator/index.js
@@ -1,17 +1,16 @@
 const display = document.getElementById("display");
 
-function appendToDisplay(input){
-    display.value += input;
+function appendToDisplay(input) {
+  display.value += input;
 }
 
-function clearDisplay(){
-    display.value = "";
-
+function clearDisplay() {
+  display.value = "";
 }
-function backSpace(){
-    display.value = display.value.substring(0, display.value.length -1);
+function backSpace() {
+  display.value = display.value.substring(0, display.value.length - 1);
 }
 
-function calculate(){
-    display.value = eval(display.value);
+function calculate() {
+  display.value = eval(display.value);
 }
