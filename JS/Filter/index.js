163f5217f24b0dd472f6edb17f19e0ba99d12b14
Breylu <mihael.p.petrov05@gmail.com>
2024-01-29 14:57:49 +0200

    Filter method

---
+++ JS/Filter/index.js
@@ -0,0 +1,32 @@
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// let evenNums = numbers.filter(isEven);
+// let oddNums = numbers.filter(isOdd);
+
+// function isEven(element) {
+//   return element % 2 === 0;
+// }
+// function isOdd(element) {
+//   return element % 2 !== 0;
+// }
+
+// const ages = [15, 17, 18, 18, 19, 20, 50];
+// const adults = age.filter(isAdult);
+// const children = age.filter(isChild);
+
+// function isAdult(element) {
+//   return element >= 18;
+// }
+// function isChild(element) {
+//   return element < 18;
+// }
+
+// const words = ["apple", "orange", "banana", "cookie", "kiwi"];
+// const shortWords = words.filter(getShortWords);
+// const longWords = words.filter(getLongWords);
+
+// function getShortWords(element) {
+//   return element.length <= 6;
+// }
+// function getLongWords(element) {
+//   return element.length > 6;
+// }
