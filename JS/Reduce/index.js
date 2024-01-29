63dd3111d5c9fa4584cef4d9326f866996a5899d
Breylu <mihael.p.petrov05@gmail.com>
2024-01-29 15:06:46 +0200

    Reduce method

---
+++ JS/Reduce/index.js
@@ -0,0 +1,22 @@
+// .reduce() = reduce the elements of an array
+//             to a single value
+
+// const prices = [3, 10, 25, 50, 30, 2];
+
+// const total = prices.reduce(sum);
+
+// function sum(previous, next) {
+//   return previous + next;
+// }
+
+// const grades = [75, 50, 90, 80, 65, 95];
+
+// const maximum = grades.reduce(getMax);
+// const minimum = grades.reduce(getMin);
+
+// function getMax(accumulator, element) {
+//   return Math.max(accumulator, element);
+// }
+// function getMin(accumulator, element) {
+//   return Math.min(accumulator, element);
+// }
