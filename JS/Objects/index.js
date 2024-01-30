c86d2f3e1e799b19b8d66b8d1065f2a8761ecb86
Breylu <mihael.p.petrov05@gmail.com>
2024-01-30 16:53:48 +0200

    This reference

--- JS/Objects/index.js
+++ JS/Objects/index.js
@@ -27,3 +27,27 @@ console.log(person2.lastName);
 console.log(person2.age);
 console.log(person2.isEmployed);
 person2.sayHello;
+
+// -------- THIS REFERENCE TO OBJECTS --------
+
+const person3 = {
+  firstName: "Spongebob",
+  lastName: "Squarepants",
+  age: 30,
+  isEmployed: true,
+  sayHello: function () {
+    console.log(`Hi! Im ${this.firstName}, im ${this.age} years old :)`);
+  },
+};
+const person4 = {
+  firstName: "Squidward",
+  favFood: "pizza",
+  age: 50,
+  isEmployed: true,
+  sayHello: function () {
+    console.log(`Hi! Im ${this.firstName}, i like ${this.favFood} :)`);
+  },
+};
+
+person3.sayHello();
+person4.sayHello();
