311a69667bc2e7fad275d0a20ee1d75974d9bdb1
Breylu <mihael.p.petrov05@gmail.com>
2024-01-31 15:36:11 +0200

    Static keyword

--- JS/Classes/index.js
+++ JS/Classes/index.js
@@ -1,26 +1,68 @@
-class Product {
-  constructor(name, price) {
-    this.name = name;
-    this.price = price;
+// class Product {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+
+//   displayProduct() {
+//     console.log(`Product: $${this.name}`);
+//     console.log(`Price: $${this.price.toFixed(2)}`);
+//   }
+
+//   calculateTotal(salesTax) {
+//     return this.price + this.price * salesTax;
+//   }
+// }
+
+// const salesTax = 0.05;
+
+// const product1 = new Product("Shirt", 19.99);
+// const product2 = new Product("Pants", 22.5);
+// const product3 = new Product("Underwear", 99.99);
+
+// product1.displayProduct();
+
+// const total = product1.calculateTotal(salesTax);
+// console.log(`Total price (with tax): $${total.toFixed(2)}`);
+
+// ------- STATIC KEYWORD -------
+
+// class mathUtil {
+//   static PI = 3.14159;
+
+//   static getDiameter(radius) {
+//     return radius * 2;
+//   }
+//   static getCircumference(radius) {
+//     return 2 * this.PI * radius;
+//   }
+// }
+
+// console.log(mathUtil.getCircumference(10));
+
+class User {
+  static userCount = 0;
+
+  constructor(username) {
+    this.username = username;
+    User.userCount++;
   }
 
-  displayProduct() {
-    console.log(`Product: $${this.name}`);
-    console.log(`Price: $${this.price.toFixed(2)}`);
+  static getUserCount() {
+    console.log(`There are ${User.userCount} users online`);
   }
-
-  calculateTotal(salesTax) {
-    return this.price + this.price * salesTax;
+  sayHello() {
+    console.log(`Hello, my username is ${this.username}`);
   }
 }
 
-const salesTax = 0.05;
+const user1 = new User("Spongebob");
+const user2 = new User("Patrick");
+const user3 = new User("Squidward");
 
-const product1 = new Product("Shirt", 19.99);
-const product2 = new Product("Pants", 22.5);
-const product3 = new Product("Underwear", 99.99);
+console.log(user1.username);
+console.log(user2.username);
+console.log(user3.username);
 
-product1.displayProduct();
-
-const total = product1.calculateTotal(salesTax);
-console.log(`Total price (with tax): $${total.toFixed(2)}`);
+user1.sayHello();
+console.log(User.username);
