db1eb447e15a58aca241cf6a143abf22b9a0fdff
Breylu <mihael.p.petrov05@gmail.com>
2024-02-08 21:42:24 +0200

    Promises

---
+++ JS/Promises/index.js
@@ -0,0 +1,53 @@
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dogWalked = true;
+
+      if (dogWalked) {
+        resolve("You walked the dog");
+      } else {
+        reject("You didn't walk the dog");
+      }
+    }, 1500);
+  });
+}
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const kitchenCleaned = false;
+
+      if (kitchenCleaned) {
+        resolve("You cleaned the kitchen");
+      } else {
+        reject("You didn't clean the kitchen");
+      }
+    }, 2500);
+  });
+}
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const trashTakenOut = true;
+      if (trashTakenOut) {
+        resolve("You took out the trash");
+      } else {
+        reject("You didn't take out the trash");
+      }
+    }, 500);
+  });
+}
+
+walkDog()
+  .then((value) => {
+    console.log(value);
+    return cleanKitchen();
+  })
+  .then((value) => {
+    console.log(value);
+    return takeOutTrash();
+  })
+  .then((value) => {
+    console.log(value);
+    console.log("You finished all the chores!");
+  })
+  .catch((error) => console.error(error));
