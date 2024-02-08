a24e1f58be8e14d8beb969b6d9cb3e02a232cde5
Breylu <mihael.p.petrov05@gmail.com>
2024-02-08 21:23:18 +0200

    Image Slider, Callbacks

--- JS/Callbacks/index.js
+++ JS/Callbacks/index.js
@@ -20,17 +20,52 @@
 //   console.log("Goodbye!");
 // }
 
-sum(displayConsole, 1, 2);
+// sum(displayConsole, 1, 2);
 
-function sum(callback, x, y) {
-  let result = x + y;
-  callback(result);
+// function sum(callback, x, y) {
+//   let result = x + y;
+//   callback(result);
+// }
+
+// function displayConsole(result) {
+//   console.log(result);
+// }
+
+// function displayPage(result) {
+//   document.getElementById("myH1").textContent = result;
+// }
+
+function task1() {
+  setTimeout(() => {
+    console.log("task 1 complete");
+    callback();
+  }, 2000);
 }
 
-function displayConsole(result) {
-  console.log(result);
+function task2() {
+  setTimeout(() => {
+    console.log("task 2 complete");
+    callback();
+  }, 1000);
 }
 
-function displayPage(result) {
-  document.getElementById("myH1").textContent = result;
+function task3() {
+  setTimeout(() => {
+    console.log("task 3 complete");
+    callback();
+  }, 3000);
 }
+
+function task4() {
+  setTimeout(() => {
+    console.log("task 4 complete");
+    callback();
+  }, 1500);
+}
+task1(() => {
+  task2(() => {
+    task3(() => {
+      task4(() => console.log("All tasks complete"));
+    });
+  });
+});
