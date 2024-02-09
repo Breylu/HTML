f160e455ce4367382269030436ddcde974c3b604
Breylu <mihael.p.petrov05@gmail.com>
2024-02-09 14:37:20 +0200

    Fetch Data

--- JS/JSON FIle/index.js
+++ JS/JSON FIle/index.js
@@ -30,14 +30,50 @@
 
 // ---------------------- FETCH ----------------------
 
-fetch("person.json")
-  .then((response) => response.json())
-  .then((value) => console.log(value));
+// fetch("person.json")
+//   .then((response) => response.json())
+//   .then((value) => console.log(value));
 
-fetch("people.json")
-  .then((response) => response.json())
-  .then((values) =>
-    values.forEach((value) => {
-      console.log(value.isEmployed);
-    })
-  );
+// fetch("people.json")
+//   .then((response) => response.json())
+//   .then((values) =>
+//     values.forEach((value) => {
+//       console.log(value.isEmployed);
+//     })
+//   );
+
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Could not fetch resourse");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((error) => console.error(error));
+
+fetchData();
+
+async function fetchData() {
+  try {
+    const pokemonName = document
+      .getElementById("pokemonName")
+      .value.toLowerCase();
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("pokemonSprite");
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+  } catch (error) {
+    console.error(error);
+  }
+}
